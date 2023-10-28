d091117ac3d1c9911d6ec027a541287fef88ed43
AnandPTi <anandpr@iitbhilai.ac.in>
2023-10-29 01:19:24 +0530

    WIP: Anands branch

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const Header = () => {
   return (
     <>
+
+    {/* My name is Ananad */}
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-col text-center w-full mb-20">
             <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
-              ASTRA
+              ANAND
             </h2>
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-100">
               CHAT WITH OUR APPLICATION
